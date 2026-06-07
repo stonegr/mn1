af082b7797363f9820c6e7093ca1502be91fea87
stonegr <74632994+stonegr@users.noreply.github.com>
2026-06-07 10:59:32 +0800

    Update default IP, hostname, and kernel version

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/LEDE/Stone/g' package/base-files/luci2/bin/config_generate
+sed -i 's/LEDE/Stone/g' package/base-files/files/bin/config_generate
 
 # openssl enable asm
 sed -i '/OPENSSL_TARGET:=linux-$(call qstrip,$(CONFIG_ARCH))-openwrt/i \
 OPENSSL_OPTIONS += enable-asm\
 OPENSSL_OPTIONS += -march=armv8-a+crypto\n' package/libs/openssl/Makefile
+
+# set linux kernal
+sed -i 's/6.6/6.1/g' target/linux/amlogic/Makefile
